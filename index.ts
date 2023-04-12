c5523a6a289bb59c2707d2458688cfe07260495c
Parmeet Singh <sparmeet162000@gmail.com>
2023-04-12 12:52:57 +0530

    package fastify/sendgrid

---
+++ index.ts
@@ -0,0 +1,38 @@
+import { FastifyPluginCallback, FastifyPluginOptions } from "fastify";
+import fp from "fastify-plugin";
+import sendgrid, { MailService } from "@sendgrid/mail";
+
+interface SendgridPluginOptions extends FastifyPluginOptions {
+  apiKey: string;
+}
+declare module "fastify" {
+  interface FastifyInstance {
+    sendgrid: MailService;
+  }
+  interface FastifyReply {
+    sendgrid: MailService;
+  }
+}
+
+const sendgridPlugin: FastifyPluginCallback<SendgridPluginOptions> = (
+  fastify,
+  options,
+  done
+) => {
+  if (fastify.sendgrid) {
+    return done(new Error("fastify-sendgrid declared"));
+  }
+
+  sendgrid.setApiKey(options.apiKey);
+
+  fastify.decorate("sendgrid", sendgrid);
+
+  done();
+};
+
+const fastifySendgrid = fp(sendgridPlugin, {
+  fastify: "4.x",
+  name: "fastify/sendgrid",
+});
+
+export default fastifySendgrid;
